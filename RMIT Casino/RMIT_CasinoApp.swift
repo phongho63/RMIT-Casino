7b62c1066b7bec52787f626120210b34b117ce10
phongho63 <s3803566@rmit.edu.vn>
2023-08-11 14:34:56 +0700

    Initial Commit

---
+++ RMIT Casino/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  RMIT Casino
+//
+//  Created by Ho Buu Quoc Phong on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
