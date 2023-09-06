dd8e7824cfdb407566ba12d67e16932b35394e28
phongho63 <s3803566@rmit.edu.vn>
2023-09-06 15:27:15 +0700

    Adding proper comments

--- RMIT Casino/Entities/PlayerDatabaseEntity.swift
+++ RMIT Casino/Entities/PlayerDatabaseEntity.swift
@@ -1,9 +1,14 @@
-//
-//  PlayerDatabaseEntity.swift
-//  RMIT Casino
-//
-//  Created by Ho Buu Quoc Phong on 06/09/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Ho Buu Quoc Phong
+  ID: s3803566
+  Created  date: 11/08/2023
+  Last modified: 06/09/2023
+  Acknowledgement: Stackoverflow, chatGPT, Youtube, Lecture slides.
+*/
 
 
 import Foundation
